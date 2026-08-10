2187453b092f616b22d35c68b5af0ee36b50fb3c
ritikkumar-18 <rk.u416@gmail.com>
2026-08-10 14:21:34 +0530

    fifth commit

---
+++ JS 1/DAY 8/LOOPS/While/Day8.js
@@ -0,0 +1,29 @@
+// While loop
+
+// print all numbers from 1 to n
+// var n=10
+// var i=1
+// while(i<=n)
+// {
+// console.log(i)
+//     i++
+
+// }
+
+// table of 8
+
+// var n=8
+// var i=1
+// while(i<=n){
+//     console.log(n*1)
+// }
+
+
+// another exmaple
+var n=3
+var i=1
+while(i<=n)
+{
+    console.log(n-i+1)
+    i++
+}
---
+++ JS 1/DAY 8/LOOPS/While/Do while/dowhile.js
@@ -0,0 +1,10 @@
+// DO while loop
+
+var i=10
+do {
+    console.log(i)
+    i++
+}
+while(i<=5){
+
+}

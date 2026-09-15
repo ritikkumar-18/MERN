c253670ae6eabe81a594720195b56d37af66f467
ritikkumar-18 <rk.u416@gmail.com>
2026-09-15 14:17:17 +0530

    26

--- JS 2/DAY 26/Spiral matrix/Leetcode problem 54/Day26.js
+++ JS 2/DAY 26/Spiral matrix/Leetcode problem 54/Day26.js
@@ -49,43 +49,53 @@
 
 // its reverse and 
 
-// var reversespiralorder=function(arr){
-//     let left=0
-//     let right=arr[0].length-1
-//     let top=0
-//     let bottom=arr.length-1
-//     let ans=[]
-//     while(left<=right && top<=bottom){
-//         for(let i=top; i<=bottom; i++){
-//             ans.push(arr[i][left])
-//         }
-//         left++
-//         if(left<=right&&top<=bottom){
+var reversespiralorder = function(arr) {
+    let left = 0;
+    let right = arr[0].length - 1;
+    let top = 0;
+    let bottom = arr.length - 1;
 
-//         for(let i=left; i<=right; i++){
-//             ans.push(arr[bottom][i])
-//         }
-//        }
-//         bottom--
-//         if(left<=right&&top<=bottom){
-//         for(let i=bottom;i>=top;i--){
-//             ans.push(arr[i][right])
-//         }
-//     }
-//         right--
-//                 if(left<=right&&top<=bottom){
-//                     for(let i=right;i>=left;i--){
-//                         ans.push(arr[top][i])
-//                     }
-//                 }
-//                 right++
+    let ans = [];
 
-//     }
-//     return ans
+    while (left <= right && top <= bottom) {
 
-// }
-// console.log(reversespiralorder([
-//     [1,2,3],
-//     [4,5,6],
-//     [7,8,9]
-// ]))
+        // Down
+        for (let i = top; i <= bottom; i++) {
+            ans.push(arr[i][left]);
+        }
+        left++;
+
+        // Right
+        if (left <= right && top <= bottom) {
+            for (let i = left; i <= right; i++) {
+                ans.push(arr[bottom][i]);
+            }
+        }
+        bottom--;
+
+        // Up
+        if (left <= right && top <= bottom) {
+            for (let i = bottom; i >= top; i--) {
+                ans.push(arr[i][right]);
+            }
+        }
+        right--;
+
+        // Left
+        if (left <= right && top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                ans.push(arr[top][i]);
+            }
+        }
+
+        top++;
+    }
+
+    return ans;
+};
+
+console.log(reversespiralorder([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]));
